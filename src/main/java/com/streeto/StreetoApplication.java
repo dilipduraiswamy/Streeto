04e3abbf216e30e7aac51b9ab83a599152ade130
dilipduraiswamy <dilipduraiswamy@gmail.com>
2023-05-21 19:56:14 +0530

    initial project

---
+++ src/main/java/com/streeto/StreetoApplication.java
@@ -0,0 +1,13 @@
+package com.streeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetoApplication.class, args);
+	}
+
+}
